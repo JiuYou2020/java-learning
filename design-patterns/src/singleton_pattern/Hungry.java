70dde2b3a1c9e38e589f261499480ac2d619425b
jiuyou2020 <jiuyou20@gmail.com>
2024-04-19 14:09:34 +0800

    es & design pattern

---
+++ design-patterns/src/singleton_pattern/Hungry.java
@@ -0,0 +1,17 @@
+package singleton_pattern;
+
+/**
+ * @author jiuyou2020
+ * @description 恶汉式单例模式
+ * @date 2024/4/19 下午12:42
+ */
+public class Hungry {
+    private static final Hungry instance = new Hungry();
+
+    private Hungry() {
+    }
+
+    public static Hungry getInstance() {
+        return instance;
+    }
+}
---
+++ design-patterns/src/singleton_pattern/Lazy.java
@@ -0,0 +1,24 @@
+package singleton_pattern;
+
+/**
+ * @author jiuyou2020
+ * @description 懒汉式单例模式
+ * @date 2024/4/19 下午12:42
+ */
+public class Lazy {
+    private static volatile Lazy instance;
+
+    private Lazy() {
+    }
+
+    public static Lazy getInstance() {
+        if (instance == null) {
+            synchronized (Lazy.class) {
+                if (instance == null) {
+                    instance = new Lazy();
+                }
+            }
+        }
+        return instance;
+    }
+}
