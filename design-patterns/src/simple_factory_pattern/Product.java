bbc6cb964a0a0cd62b041cf21355098187e13b3a
jiuyou2020 <jiuyou20@gmail.com>
2024-04-22 15:16:30 +0800

    design pattern

---
+++ design-patterns/src/simple_factory_pattern/Product.java
@@ -0,0 +1,20 @@
+package simple_factory_pattern;
+
+/**
+ * @author jiuyou2020
+ * @description 基类：抽象类
+ * @date 2024/4/22 上午10:57
+ */
+public abstract class Product {
+    /**
+     * 所有产品类的公共业务方法
+     */
+    public void methodSame() {
+        System.out.println("所有产品类的公共业务方法");
+    }
+
+    /**
+     * 声明抽象业务方法，由子类实现
+     */
+    public abstract void methodDiff();
+}
