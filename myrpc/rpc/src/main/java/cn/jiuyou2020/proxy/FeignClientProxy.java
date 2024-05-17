3bbf9ebc485a9da9b3d7cddd4f05fb158485db45
jiuyou2020 <jiuyou20@gmail.com>
2024-05-17 11:48:29 +0800

    command-pattern upload and The dynamic proxy part of myrpc is partially completed.

--- myrpc/rpc/src/main/java/cn/jiuyou2020/proxy/FeignClientProxy.java
+++ myrpc/rpc/src/main/java/cn/jiuyou2020/proxy/FeignClientProxy.java
@@ -1,13 +1,10 @@
 package cn.jiuyou2020.proxy;
 
-import org.springframework.beans.BeansException;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
-
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
+@SuppressWarnings("unchecked")
 public class FeignClientProxy implements InvocationHandler {
     private FeignClientFactoryBean feignClientFactoryBean;
 
@@ -17,21 +14,9 @@ public class FeignClientProxy implements InvocationHandler {
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) {
-        //获取url
-
-        // 在这里根据方法名称和参数实现具体的业务逻辑
-        if (method.getName().equals("getUser")) {
-            // 执行获取用户的业务逻辑
-            return getUser(args);
-        }
-        // 其他方法的处理逻辑
-        return null;
-    }
-
-    private Object getUser(Object[] args) {
-        // 实现获取用户的逻辑
-        // 这里可以调用你的业务服务或者模拟返回用户数据
-        return "User information";
+        //获取url,模拟远程调用
+        String url = feignClientFactoryBean.getUrl() + "/";
+        return url + method.getName();
     }
 
     public static <T> T createProxy(Class<T> interfaceClass, FeignClientFactoryBean feignClientFactoryBean) {
